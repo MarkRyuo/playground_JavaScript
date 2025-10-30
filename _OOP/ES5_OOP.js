8b282200cc035f09f6e8ff30ba797de3551452b8
Riyuo <arcx@Riyuos-MacBook-Air.local>
2025-10-30 15:54:00 +0800

    Subject: Update

---
+++ _OOP/ES5_OOP.js
@@ -0,0 +1,12 @@
+
+
+function Book(name, author, year){
+
+    this.name = name;
+    this.author = author;
+    this.year = year;
+
+}
+
+const Book1 = new Book("Book1", "Jane Doe", 2003)
+console.log(Book1)
