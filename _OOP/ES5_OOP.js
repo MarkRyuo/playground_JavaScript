7416f0b863276bda6af825d5fa9a8b079e56b20c
Riyuo <arcx@Riyuos-MacBook-Air.local>
2025-10-30 16:55:21 +0800

    Subject: Update

--- _OOP/ES5_OOP.js
+++ _OOP/ES5_OOP.js
@@ -1,12 +1,45 @@
 
-
+// Encapsulation & Abstraction
 function Book(name, author, year){
-
+    
+    if(this.constructor === Book){
+        throw new Error("Cannot instantiate abstract class 'Book'")
+    }
     this.name = name;
     this.author = author;
     this.year = year;
-
 }
 
-const Book1 = new Book("Book1", "Jane Doe", 2003)
-console.log(Book1)
+Book.prototype.display = function(){
+    throw new Error("Abstract method 'display()' must be implemented by subclass")
+}
+
+
+// Inheritance 
+function Magazine(name, author, year, yearend){
+    Book.call(this, name, author, year)
+    this.yearend = yearend;
+    
+}
+
+
+//Polymorphism 
+Magazine.prototype.display = function(){
+    return `Book Name: ${this.name}, Author Name: ${this.author}, Year Created: ${this.year}, Year End: ${this.yearend}`
+}// function expression
+
+//Initialize
+const Book2 = new Magazine("Book1", "Jane Doe", 2003, 2004);
+console.log(Book2.display())
+
+
+//Object & Object Property
+
+//Object.create()
+//Object.keys()
+//Object.values()
+//Object.entries()
+//Object.assign()
+//Object.freeze()
+//Object.seal()
+//Object.getPrototypeOf()
