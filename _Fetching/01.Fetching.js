57e53eecf2f6766f8d7fd6ecefc6a49e26b4ac54
Riyuo <arcx@Riyuos-MacBook-Air.local>
2025-10-30 17:02:54 +0800

    Subject: Update

---
+++ _Fetching/01.Fetching.js
@@ -0,0 +1,11 @@
+
+
+//Regular Vanilla Js Fetching
+
+fetch("http://sample/v1/api", {
+    method: "GET"
+})
+    .then() // response
+    .then() // data
+    .catch() // catch the error
+    .finally() // Always log
