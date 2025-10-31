5c6ab41a22fc0bbf4a7e9d26b15143e07e06a588
MarkRyuo <malupajhonmark3035@gmail.com>
2025-10-31 11:06:03 +0800

    playgrounJavaScript: Update by Windows

--- _Fetching/01.Fetching.js
+++ _Fetching/01.Fetching.js
@@ -1,11 +1,18 @@
 
 
-//Regular Vanilla Js Fetching
+//Static Vanilla Js Fetching
 
 fetch("http://sample/v1/api", {
     method: "GET"
 })
-    .then() // response
-    .then() // data
+    .then(response => {
+        
+        if(!response.ok) throw new Error(`http error status: ${response.status}`)
+        return response.json() // Parse the json data
+    })
+    .then(data => {
+        
+        cons
+    }) // data
     .catch() // catch the error
     .finally() // Always log
