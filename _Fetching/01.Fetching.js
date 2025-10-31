257198c3ca5dca92a16da50df17cff77eda3f059
Riyuo <arcx@Riyuos-MacBook-Air.local>
2025-10-31 11:16:17 +0800

    Subject: Update

--- _Fetching/01.Fetching.js
+++ _Fetching/01.Fetching.js
@@ -7,11 +7,13 @@ fetch("https://pokeapi.co/api/v2/pokemon/ditto", {
 })
     .then(response => {
         
-        if(!response.ok) throw new Error(`http error status: ${response.status}`) //check if response is ok
+        if(!response.ok) {
+            throw new Error(`http error status: ${response.status}`) //check if response is ok   
+        }
         return response.json() // Parse the json data
     })
     .then(data => {
-        console.log(data)
+        console.log(data.forms[0].name)
     }) // data
     .catch(error => console.error(error.message)) // catch the error
     .finally() // Always log
