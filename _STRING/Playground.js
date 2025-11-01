75ea8b44fb3024b7f41d84b0de38c6111eb7a896
Riyuo <arcx@Riyuos-MacBook-Air.local>
2025-11-02 03:06:05 +0800

    Subject: Update

---
+++ _STRING/Playground.js
@@ -0,0 +1,13 @@
+
+
+// Search String Methods 
+
+const str = "Hello, I'm Riyuo a normal Programmer. It's me Riyou";
+
+//Find the index of Riyuo
+console.log(str.indexOf("Riyuo"))
+
+//Check if existing the name Riyuo in the string 
+console.log(str.includes("Riyuo"))
+
+
