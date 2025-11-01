23a28c2b267c35f1222357a036a2251350294d6b
Riyuo <arcx@Riyuos-MacBook-Air.local>
2025-11-02 02:57:56 +0800

    Subject: Update

--- _STRING/01_strings.js
+++ _STRING/01_strings.js
@@ -15,12 +15,8 @@ text.startsWith("The") // return true otherwise false
 text.search("lazy") // search string by string  Output 34
 text.includes("fox"); // return true otherwise false 
 text2.matchAll("locate") // return iterator containing the result of matching 
-text.match("uick") // return an array contain a result /uick/g 
+text2.match("locate") // return an array contain a result. RegExp: /locate/g 
 
 // returning index
-text2.indexOf("locate"); // return the index otherwise -1 text not found output: 7
-text.lastIndexOf("locate") // return the index output: 21
-
-
-
-
+text2.indexOf("locate"); // return the index otherwise -1 text not found. output: 7
+text2.lastIndexOf("locate") // return the index otherwise -1. output: 21
