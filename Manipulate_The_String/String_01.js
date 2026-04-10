20bfc7f899fb961499f4a68f914574498758a3c8
MarkRyuo <malupajhonmark3035@gmail.com>
2026-04-11 07:57:06 +0800

    Subject: Update

---
+++ Manipulate_The_String/String_01.js
@@ -0,0 +1,6 @@
+
+// What do you need to know? Manipulate the string 
+// Where can i use it? Interview 
+
+
+// There have many methods for manipulate the string but the most important and most use is: 
